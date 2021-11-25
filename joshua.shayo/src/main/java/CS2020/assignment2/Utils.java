ab12b9b43979ee82020a3baddf8959239f1c1f0a
JOSHUA SHAYO <j.shayo.20@abdn.ac.uk>
2021-11-25 20:31:56 +0000

    Task 2 complete

--- joshua.shayo/src/main/java/CS2020/assignment2/Utils.java
+++ joshua.shayo/src/main/java/CS2020/assignment2/Utils.java
@@ -3,6 +3,8 @@ package CS2020.assignment2;
 import java.util.ArrayList;
 import java.util.UUID;
 import java.util.HashMap;
+import java.time.LocalDate;
+import java.time.DayOfWeek;
 
 public class Utils
 {
@@ -13,6 +15,11 @@ public class Utils
             songHashMap.put(song.getSongID(), song.getTitle() + " (" + song.getDuration()/60 + ":" + song.getDuration()%60 + ")");
         }
         return songHashMap;
+    }
+    
+    public static boolean checkIfBornOnWeekend(String dobOfArtist){
+        DayOfWeek day = LocalDate.parse(dobOfArtist).getDayOfWeek();
+        return day.equals("SATURDAY") || day.equals("SUNDAY");
     }
     
 }
